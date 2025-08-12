4b5b11dc8b7da1c14f536e505d8097c591344e4e
Vishal Kumar <iamkrvishal07@gmail.com>
2025-08-12 08:32:00 +0530

    structs

--- 16_structs/struct.go
+++ 16_structs/struct.go
@@ -7,49 +7,41 @@ import (
 
 //order struct <blueprint>
 
-type order struct{
-	id     string
-	amount float32
-	status string
-	createdAt time.Time   // time package inbuilt -> nanosecond precision
+type order struct {
+	id        string
+	amount    float32
+	status    string
+	createdAt time.Time // time package inbuilt -> nanosecond precision
 }
 
-
 // receiver type
-func (o *order)changeStatus(status string){
-	o.status=status
+func (o *order) changeStatus(status string) {
+	o.status = status
 	// struct automatically do deferencing work.....
 
 }
 
-func (o *order)getAmout() float32{
+func (o *order) getAmout() float32 {
 	return o.amount
 	// yaha * use kr v skte the nhi v [ agar value change krna he toh * use kro best ]
 	// yaha (o order) v likhe toh kaam karega .
 }
 
-
-
-func newOrder(id string, amount float32, status string)*order {
+func newOrder(id string, amount float32, status string) *order {
 	// initial setup goes here...
-	myOrder :=order{
-		id: id,
+	myOrder := order{
+		id:     id,
 		amount: amount,
 		status: status,
 	}
 
 	return &myOrder
 
-
 }
-
-
-
 
 func main() {
 
-
-	myOrder := newOrder("1",30.50,"received")
+	myOrder := newOrder("1", 30.50, "received")
 	fmt.Println(myOrder)
 
 	// ** if you don't set any field, default value is zero value
@@ -69,16 +61,14 @@ func main() {
 	// fmt.Println(myOrder.amount)
 	// fmt.Println("Order struct", myOrder)
 
-
 	// 	myOrder := order{
 	// 	id: "1",
 	// 	amount: 70.00,
 	// 	status: "pending",
 	// }
 
-
-		myOrder := order{
-		id: "1",
+	myOrder := order{
+		id:     "1",
 		amount: 70.00,
 		status: "pending",
 	}
@@ -87,5 +77,16 @@ func main() {
 	myOrder.changeStatus("confirmed")
 	fmt.Println(myOrder)
 	fmt.Println(myOrder.getAmout())
+
+
+
+	// kabhi kabhi ek baar struct construct krte hai tab kya krna chahiye--------------
+
+	language :=struct{
+		name string
+		isGood bool
+	}{"Hello",flase}
+
+	fmt.Println(language)
 
 }
